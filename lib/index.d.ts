0a5a2f5f7c3bdf8d68561c14365f6487b3e2b7a3
Stanislau Karaban <stanislau.karaban@immfly.com>
2020-10-22 13:06:38 +0300

    Typescript

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+declare const _default: {
+    numbers: (l?: number) => string | number;
+    letters: (l?: number) => string | number;
+    strings: (l?: number) => string | number;
+};
+export default _default;
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+const eventseed: number[] = [
+  ((new Date()).getTime() * 41 ^ (Math.random() * Math.pow(2, 32))) >>> 0,
+  ((new Date()).getTime() * 41 ^ (Math.random() * Math.pow(2, 32))) >>> 0,
+  ((new Date()).getTime() * 41 ^ (Math.random() * Math.pow(2, 32))) >>> 0,
+  ((new Date()).getTime() * 41 ^ (Math.random() * Math.pow(2, 32))) >>> 0
+]
+
+const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const letters: string[] = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+const strings: (string|number)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+const calc = (chars: (string|number)[], length: number): (string | number) => {
+  let random = ''
+  for (var i = length; i > 0; --i) {
+    random += chars[Math.abs((Math.floor(Math.random() * Math.pow(2, 32)) ^ (eventseed[i % 4] >>> (i % 23))) % (chars.length - 1 + 1))]
+  }
+  return random
+}
+
+export default {
+  numbers: (l: number = 4) => calc(numbers, l),
+  letters: (l: number = 4) => calc(letters, l),
+  strings: (l: number = 6) => calc(strings, l)
+}
